b3068cbf68d80b34a6bb33f87c6f5b47817279b2
sslucasmv <lmvolpe24@gmail.com>
2024-07-15 14:21:01 -0300

    subida navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Footer from './components/layouts/Footer'
+import Navbar from './components/layouts/Navbar'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>Pagina</h1>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/"/>
+    </Routes>
+   </BrowserRouter>
+    <Navbar />
      <Footer />
     </>
   )
--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,8 +4,8 @@
 const Footer = () => {
     return (
     <footer className="footer">
-        <div className="container-center footer-align">
-        <p>Todos los derechos reservados &copy; 2022 URBAN SHOP</p>
+        <div className="container-center footer-align ">
+        <p>Todos los derechos rleservados &copy; 2022 URBAN SHOP</p>
         </div>
     </footer>
     )
